fe9a9f7e0ab6da42a423cf3b60d2913d6a40a725
Daniel Hernandez <94863518+VaquitaCosmica@users.noreply.github.com>
2022-06-13 17:28:30 -0500

    fisrt commit

---
+++ src/main/java/com/mycompany/biblioteca_mariana/JAXRSConfiguration.java
@@ -0,0 +1,13 @@
+package com.mycompany.biblioteca_mariana;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+/**
+ * Configures JAX-RS for the application.
+ * @author Juneau
+ */
+@ApplicationPath("resources")
+public class JAXRSConfiguration extends Application {
+    
+}
